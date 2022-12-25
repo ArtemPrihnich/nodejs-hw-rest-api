958e2001e00df26f57d1f71332392962da3b47a2
ArtemPrihnich <artem.prihnich@gmail.com>
2022-12-25 14:07:00 +0200

    добавлен фильтр по значению favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,10 +4,12 @@ const getAll = async (req, res, next) => {
   try {
     const { _id: owner } = req.user
 
-    const { page = 1, limit = 10 } = req.query
+    const { page = 1, limit = 10, favorite } = req.query
     const skip = (page - 1) * limit
 
-    const contacts = await Contact.find({owner}, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'email')
+    const favoriteFilter = favorite ? favorite : { $in: [true, false] }
+
+    const contacts = await Contact.find({ owner, favorite: favoriteFilter }, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'email')
     
     res.json(contacts)
   } catch (error) {
