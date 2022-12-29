2f3a4efd227f3ee1607580a63d9d82d53816786a
ArtemPrihnich <artem.prihnich@gmail.com>
2022-12-29 13:28:13 +0200

    добавлено изменение размеров загружаемого изображения

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,5 @@
-const fs = require('fs/promises')
 const path = require('path')
-// const Jimp = require('jimp')
+const resizeImage = require('../../utils')
 
 const User = require('../../models/user')
 
@@ -12,14 +11,10 @@ const updateAvatar = async(req, res, next) => {
         const { _id } = req.user
         const filename = `${_id}_${originalname}`
         const resultUpload = path.join(avatarsDir, filename)
-        // console.log(tempUpload)
+       
+        await resizeImage(tempUpload, resultUpload)
 
-        // const test = await Jimp.read(`${tempUpload}`).then(change => change.resize(250, 250).write(filename)).catch(err => console.log(err))
-        // console.log(test)
-
-        await fs.rename(tempUpload, resultUpload)
-
-        const avatarUrl = path.join('avatars', originalname)
+        const avatarUrl = path.join('avatars', filename)
 
         await User.findByIdAndUpdate(_id, { avatarUrl })
         
